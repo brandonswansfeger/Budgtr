0b3fb4433ed8c8d4cc13b37fabe1a96b44233308
brandonswansfeger <88030396+brandonswansfeger@users.noreply.github.com>
2021-10-21 07:19:05 -0400

    set-up serever and created index route

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+// =======================================
+//              DATABASE
+// =======================================
+const budget = require('./models/budget.js');
+
+app.use(express.static('public'));
+
+// =======================================
+//              ROUTES
+// =======================================
+// index route
+app.get('/budget/', (req, res) => {
+  res.send(budget);
+});
+
+// show route
+app.get('/budget/:id', (req, res) => {
+  res.send(budget[req.params.id]);
+});
+
+// =======================================
+//              LISTENER
+// =======================================
+app.listen(port, () => {
+  console.log(`Budgtr app listening on port: ${port}`)
+});
